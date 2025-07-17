42fdb109a0726a0fedccf347ee7bd2c07618a8ed
Alejandro Reyes <reyesbalejandroe@gmail.com>
2025-07-17 14:11:41 +0200

    update/intial_files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
+
+// Necesario exportar app para que se puedan ejecutar los tests correctamente
+module.exports = app;
